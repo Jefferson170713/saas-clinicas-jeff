3c58787ae84c74a80f8b746e4ccb125caebc898c
Jefferson de almeida <105664223+Jefferson170713@users.noreply.github.com>
2026-02-11 18:11:37 +0000

    feat(express): Inicializando um servidor simples com o express

---
+++ modules/express.js
@@ -0,0 +1,28 @@
+const express = require('express');
+
+const port = 5324;
+
+const app = express();
+
+app.get('/home', (req, res) => {
+    res.status(200).send("<h1>Testtando uma home page com express</h1>");
+})
+
+app.get('/users', (req, res) => {
+    const users = [
+        {
+            name: "Jefferson",
+            age: 32
+        },
+        {
+            name: "Maria Rayssa",
+            age: 29
+        }
+    ];
+    res.status(200).json(users);
+});
+
+app.listen(port, () => {
+    console.log(`Servidor rodando na porta: ${port}`);
+    console.log(`Acesse: http://localhost:${port}/home`);
+});
